556a7fbd458aedc00bf1ed41daed4d8fd70effa1
abhidahal <abhidahal7@gmail.com>
2023-03-02 11:17:56 +0545

    frame back ground change

--- config/frameData.js
+++ config/frameData.js
@@ -1,7 +1,7 @@
 /* eslint-disable max-len */
 export default {
   frames: {
-    ONE: 'https://res.cloudinary.com/dwrou0v5e/image/upload/v1677695480/badge_1_rdwonr.png',
-    TWO: 'https://res.cloudinary.com/dfwoltdf5/image/upload/v1670422804/HackNITR/Frames/badge_2.png',
+    ONE: 'https://res.cloudinary.com/dwrou0v5e/image/upload/v1677735059/badge_1_c7ivln.png',
+    TWO: 'https://res.cloudinary.com/dwrou0v5e/image/upload/v1677695480/badge_1_rdwonr.png',
   },
 };
